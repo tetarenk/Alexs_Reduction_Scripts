81195ff6664d9c1c19cea5cb1c53ce28ce9ffd5a
Alexandra Tetarenko <atetarenk@Alexandra-Tetarenkos-MacBook-Pro.local>
2017-03-26 10:51:28 -0600

    added comments

--- jcmt_scuba2_cube_fit.py
+++ jcmt_scuba2_cube_fit.py
@@ -94,36 +94,42 @@ print 'Reading in parameters...'
 ####################################
 #User input
 ####################################
-my_dir='/Users/atetarenk/Desktop/V404_mm_radio_timing_res/jcmt_timing/'
+my_dir='/Users/atetarenk/Desktop/V404_mm_radio_timing_res/jcmt_timing/'#input/output directory
 band=850
 date='jun22'
+target='v404cyg'
 do_timing='y'#'n' only fits full data set
+psf_string8=my_dir+'out_crl2688_32_cal_psf.fits'#calibrator fits file at 850
+psf_string4=my_dir+'out_crl2688_32_cal4_psf.fits'#calibrator fits file at 450
+#NOTE: target maps at 850 (450) need to have the following name convention:
+#cube-->[my_dir][target]_[scan]_shortmaps_cube_cal(4).fits
+#full map-->[my_dir][target]_[scan]_fullmap_cal(4).fits
 #########
 scan_list8=[36,37,41,42,46,47,52]#scan numbers at 850
 scan_list4=[36,37,41,42,46]#scan numbers at 450
 w850=1.63#width of gaussian beam in pixels at 850
 w450=2.17#width of gaussian beam in pixels at 450
-ranges8=[[123,133,129,139],[122,132,128,138],[110,125,118,135],[120,130,125,135],[110,135,130,155],\
-[93,123,116,146],[107,123,121,137]]#pixel ranges x1,y1,x2,y2 at 850
+ranges8=[[123,133,129,139],[122,132,128,138],[110,125,118,135],[120,130,125,135],\
+[110,135,130,155],[93,123,116,146],[107,123,121,137]]#pixel ranges x1,x2,y1,y2 at 850 scans
 ranges4=[[252,262,267,275],[250,260,266,276],[240,250,260,270],\
-[240,250,245,255],[243,253,278,288]]#pixel ranges x1,y1,x2,y2 at 450
-full_fixed_params=np.array([True, False, False, False, False,False, True], dtype=bool)
+[240,250,245,255],[243,253,278,288]]#pixel ranges x1,x2,y1,y2 at 450 scans
+full_fit_guess8=[0.0,1.0,1.0,1.0,w850,w850,0.0]#height,amplitude,X,Y,width_x,width_y,rota
+full_fit_guess4=[0.0,1.0,1.0,1.0,w450,w450,0.0]#height,amplitude,X,Y,width_x,width_y,rota
+full_fixed_params=np.array([True, False, False, False, True,True, True], dtype=bool)
 cube_fixed_params=np.array([True, False, True,True,True,True,True], dtype=bool)
-#calibrator scan
-psf_dim8=[0,48,0,48]#dimensions of calibrator scan at 850
-psf_dim4=[0,50,0,50]#dimensions of calibrator scan at 450
+#calibrator scan (used as a check on results from starlink only)
+psf_dim8=[0,48,0,48]#dimensions of calibrator scan at 850; x1,x2,y1,y2
+psf_dim4=[0,50,0,50]#dimensions of calibrator scan at 450; x1,x2,y1,y2
 psf_fit_guess8=[0.0,0.8,24,24,1.0,1.0,0.0]#height,amplitude,X,Y,width_x,width_y,rota
-psf_string8=my_dir+'out_crl2688_32_cal_psf.fits'#calibrator fits file
-full_fit_guess8=[0.0,1.0,1.0,1.0,1.63,1.63,0.0]#height,amplitude,X,Y,width_x,width_y,rota
 psf_fit_guess4=[0.0,0.8,25,25,1.0,1.0,0.0]#height,amplitude,X,Y,width_x,width_y,rota
-psf_string4=my_dir+'out_crl2688_32_cal4_psf.fits'#or54
-full_fit_guess4=[0.0,1.0,1.0,1.0,2.17,2.17,0.0]#height,amplitude,X,Y,width_x,width_y,rota
 psf_fixed_params=np.array([True, False, False, False, False, False, True], dtype=bool)
-#for aperature photometry, just used as a check against gaussian fit!!
-flux_ratio8=[2.359/630.371,2.359/630.371,2.359/630.371,2.359/630.371,2.347/645.264,2.347/645.264,2.347/645.264]#FCF_aperature/FCF_beam
-flux_ratio4=[7.873/641.625,7.873/641.625,7.873/641.625,7.873/641.625,9.701/638.043]#FCF_aperature/FCF_beam
-pixrad8=1.2#aperature size in pixels
-pixrad4=1.4#aperature size in pixels
+#for aperature photometry (used as a check against gaussian fit only)
+flux_ratio8=[2.359/630.371,2.359/630.371,2.359/630.371,2.359/630.371,2.347/645.264,\
+2.347/645.264,2.347/645.264]#FCF_aperature/FCF_beam at 850
+flux_ratio4=[7.873/641.625,7.873/641.625,7.873/641.625,\
+7.873/641.625,9.701/638.043]#FCF_aperature/FCF_beam at 450
+pixrad8=1.2#aperature size (radius) in pixels at 850
+pixrad4=1.4#aperature size (radius) in pixels at 450
 #########################################
 
 
@@ -177,8 +183,8 @@ elif band==450:
 for j in range(0,len(scan_list)):
 	#fits file names
 	if band==850:
-		shortmap_string=my_dir+'v404cyg_'+str(scan_list[j])+'_shortmaps_cube_cal.fits'
-		full_string=my_dir+'v404cyg_'+str(scan_list[j])+'_fullmap_cal.fits'
+		shortmap_string=my_dir+target+'_'+str(scan_list[j])+'_shortmaps_cube_cal.fits'
+		full_string=my_dir+target+'_'+str(scan_list[j])+'_fullmap_cal.fits'
 		#read in data cube, full map psf and full map
 		print 'Reading in fits files...'
 		data,type_data,shape_data,var=read_data_cube(shortmap_string,'cube')
@@ -189,8 +195,8 @@ for j in range(0,len(scan_list)):
 			var_full[0,ranges8[j][0]:ranges8[j][1],ranges8[j][2]:ranges8[j][3]],full_fit_guess8,\
 			full_fixed_params,'t')
 	elif band==450:
-		shortmap_string=my_dir+'v404cyg_'+str(scan_list[j])+'_shortmaps_cube_cal4f.fits'
-		full_string=my_dir+'v404cyg_'+str(scan_list[j])+'_fullmap_cal4.fits'
+		shortmap_string=my_dir+target+'_'+str(scan_list[j])+'_shortmaps_cube_cal4.fits'
+		full_string=my_dir+target+'_'+str(scan_list[j])+'_fullmap_cal4.fits'
 		#read in data cube, full map psf and full map
 		print 'Reading in fits files...'
 		data,type_data,shape_data,var=read_data_cube(shortmap_string,'cube')
@@ -296,4 +302,6 @@ if do_timing=='y':
 	plt.legend(loc='upper left',numpoints=1)
 	plt.show()
 
+print '*************************'
 print 'Script is finished.'
+print '*************************'
